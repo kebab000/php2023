a781eab3b90639af163b31e7b1b3cf73d9dc8fe8
kebab000 <dldytpq7010@gmail.com>
2023-05-16 14:09:43 +0900

    2023.05.16

--- php/blog/blog.php
+++ php/blog/blog.php
@@ -1,63 +1,52 @@
+<?php 
+    include "../connect/connect.php";
+    include "../connect/session.php";
+
+
+    $sql = "SELECT count(boardID) FROM board";
+    $result = $connect -> query($sql);
+    $boardTotalCount = $result -> fetch_array(MYSQLI_ASSOC);
+    $boardTotalCount = $boardTotalCount['count(boardID)'];
+?>
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP 블로그 메인</title>
+    <html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>블로그</title>
 
-    <!-- CSS -->
-    <link rel="stylesheet" href="assets/css/style.css">
+        <?php include "../include/head.php" ?>
+    </head>
+    <body class="gray">
 
-    <!-- SCRIPT -->
-    <script defer src="assets/js/common.js"></script>
-    
-</head>
-<body class="gray">
-    
-    <div id="skip">
-        <a href="#header">헤더 영역 바로가기</a>
-        <a href="#main">컨텐츠 영역 바로가기</a>
-        <a href="#footer">푸터 영역 바로가기</a>
-    </div>
-    <!-- //skip -->
+        <?php include "../include/skip.php" ?>
+        <!-- //skip -->
 
-    <header id="header" class="container">
-        <div class="header__inner">
-            <div class="left">
-                <a href="../index.html" class="star">
-                    <span class="blind">메인으로</span>
-                </a>
-            </div>
-            <h1 class="logo"> <a href="main.html">Developer Blog</a></h1>
-            <div class="right">
-                <ul>
-                    <li><a href="join.html">회원가입</a></li>
-                </ul>
-            </div>
-        </div>
-        <nav class="nav__inner">
-            <ul>
-                <li><a href="join.html">회원가입</a></li>
-                <li><a href="login.html">로그인</a></li>
-                <li><a href="board.html">게시판</a></li>
-                <li><a href="blog.html">블로그</a></li>
-            </ul>
-        </nav>
-    </header>
-    <!-- //header -->
-
-    <main id="main" class="container">
+        <?php include "../include/header.php" ?>
+        <!-- //header -->
+        <main id="main" class="container">
         <div class="blog__search bmStyle">
             <h2>개발자 블로그 입니다.</h2>
-                <p> 개발과 관련된 글을 작성해주세요</p>
+            <p> 개발과 관련된 글을 작성해주세요</p>
             <div class="search">
+            <?php if(isset($_SESSION['memberID'])){ ?>  
+                <!-- 로그인을 한 경우 -->
                 <form action="#" name="#" method="POST">
                     <legend class="blind">블로그 검색</legend>
                     <input type="search" name="searchKeyword" aria-label="검색" placeholder="검색어를 입력하세요" class="inputStyle2">
                     <button type="submit" class="btnStyle4">검색하기</button>
-                    <a href="boardWrite.html" class="btnStyle4 white">글쓰기</a>
+                    <a href="blogWrite.php" class="btnStyle4 white">글쓰기</a>
                 </form>
+            <?php }else{ ?>  
+                <!-- 로그인이 안된 경우 -->
+                <form action="#" name="#" method="POST">
+                    <legend class="blind">블로그 검색</legend>
+                    <input type="search" name="searchKeyword" aria-label="검색" placeholder="검색어를 입력하세요" class="inputStyle2">
+                    <button type="submit" class="btnStyle4">검색하기</button>
+                </form>
+            <?php } ?>    
+                
             </div>
         </div>
         <div class="blog__inner">
@@ -65,11 +54,11 @@
                
                 <div class="blog__wrap">
                     <h2>All Post</h2>
-                    <div class="cards__inner col3 line3">
+                    <div class="cards__inner col2 line3">
                         <div class="card">
                             <figure class="card__img">
-                                <source srcset="assets/img/blog02.jgp, assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, assets/img/blog02@3x.jgp 3x" />
-                                <img src="assets/img/blog02.jpg" alt="소개이미지">
+                                <source srcset="../assets/img/blog02.jgp, ../assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, ../assets/img/blog02@3x.jgp 3x" />
+                                <img src="../assets/img/blog02.jpg" alt="소개이미지">
                             </figure>
                             <div class="card__title">
                                 <h3>React에서 Redux를 사용하는 방법</h3>
@@ -81,8 +70,8 @@
                         </div>
                         <div class="card">
                             <figure class="card__img">
-                                <source srcset="assets/img/blog02.jgp, assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, assets/img/blog02@3x.jgp 3x" />
-                                <img src="assets/img/blog02.jpg" alt="소개이미지">
+                                <source srcset="../assets/img/blog02.jgp, ../assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, ../assets/img/blog02@3x.jgp 3x" />
+                                <img src="../assets/img/blog02.jpg" alt="소개이미지">
                             </figure>
                             <div class="card__title">
                                 <h3>React에서 Redux를 사용하는 방법</h3>
@@ -94,8 +83,8 @@
                         </div>
                         <div class="card">
                             <figure class="card__img">
-                                <source srcset="assets/img/blog02.jgp, assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, assets/img/blog02@3x.jgp 3x" />
-                                <img src="assets/img/blog02.jpg" alt="소개이미지">
+                                <source srcset="../assets/img/blog02.jgp, ../assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, ../assets/img/blog02@3x.jgp 3x" />
+                                <img src="../assets/img/blog02.jpg" alt="소개이미지">
                             </figure>
                             <div class="card__title">
                                 <h3>React에서 Redux를 사용하는 방법</h3>
@@ -107,8 +96,8 @@
                         </div>
                         <div class="card">
                             <figure class="card__img">
-                                <source srcset="assets/img/blog02.jgp, assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, assets/img/blog02@3x.jgp 3x" />
-                                <img src="assets/img/blog02.jpg" alt="소개이미지">
+                                <source srcset="../assets/img/blog02.jgp, ../assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, ../assets/img/blog02@3x.jgp 3x" />
+                                <img src="../assets/img/blog02.jpg" alt="소개이미지">
                             </figure>
                             <div class="card__title">
                                 <h3>React에서 Redux를 사용하는 방법</h3>
@@ -120,8 +109,8 @@
                         </div>
                         <div class="card">
                             <figure class="card__img">
-                                <source srcset="assets/img/blog02.jgp, assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, assets/img/blog02@3x.jgp 3x" />
-                                <img src="assets/img/blog02.jpg" alt="소개이미지">
+                                <source srcset="../assets/img/blog02.jgp, ../assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, ../assets/img/blog02@3x.jgp 3x" />
+                                <img src="../assets/img/blog02.jpg" alt="소개이미지">
                             </figure>
                             <div class="card__title">
                                 <h3>React에서 Redux를 사용하는 방법</h3>
@@ -133,47 +122,8 @@
                         </div>
                         <div class="card">
                             <figure class="card__img">
-                                <source srcset="assets/img/blog02.jgp, assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, assets/img/blog02@3x.jgp 3x" />
-                                <img src="assets/img/blog02.jpg" alt="소개이미지">
-                            </figure>
-                            <div class="card__title">
-                                <h3>React에서 Redux를 사용하는 방법</h3>
-                                <p class="three-line">이 글에서는 React 애플리케이션에서 Redux를 사용하여 상태를 관리하는 방법을 설명합니다. Redux의 기본 개념부터 간단한 예제까지 다룹니다.</p>
-                            </div>
-                            <div class="card__info">
-                                <a href="#" class="more">더보기</a>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <figure class="card__img">
-                                <source srcset="assets/img/blog02.jgp, assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, assets/img/blog02@3x.jgp 3x" />
-                                <img src="assets/img/blog02.jpg" alt="소개이미지">
-                            </figure>
-                            <div class="card__title">
-                                <h3>React에서 Redux를 사용하는 방법</h3>
-                                <p class="three-line">이 글에서는 React 애플리케이션에서 Redux를 사용하여 상태를 관리하는 방법을 설명합니다. Redux의 기본 개념부터 간단한 예제까지 다룹니다.</p>
-                            </div>
-                            <div class="card__info">
-                                <a href="#" class="more">더보기</a>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <figure class="card__img">
-                                <source srcset="assets/img/blog02.jgp, assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, assets/img/blog02@3x.jgp 3x" />
-                                <img src="assets/img/blog02.jpg" alt="소개이미지">
-                            </figure>
-                            <div class="card__title">
-                                <h3>React에서 Redux를 사용하는 방법</h3>
-                                <p class="three-line">이 글에서는 React 애플리케이션에서 Redux를 사용하여 상태를 관리하는 방법을 설명합니다. Redux의 기본 개념부터 간단한 예제까지 다룹니다.</p>
-                            </div>
-                            <div class="card__info">
-                                <a href="#" class="more">더보기</a>
-                            </div>
-                        </div>
-                        <div class="card">
-                            <figure class="card__img">
-                                <source srcset="assets/img/blog02.jgp, assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, assets/img/blog02@3x.jgp 3x" />
-                                <img src="assets/img/blog02.jpg" alt="소개이미지">
+                                <source srcset="../assets/img/blog02.jgp, ../assets/img/blog02.jgp, assets/img/blog02@2x.jgp 2x, ../assets/img/blog02@3x.jgp 3x" />
+                                <img src="../assets/img/blog02.jpg" alt="소개이미지">
                             </figure>
                             <div class="card__title">
                                 <h3>React에서 Redux를 사용하는 방법</h3>
@@ -190,8 +140,8 @@
                 <div class="blog__aside">
                     <div class="intro">
                         <picture class="img">
-                            <source srcset="assets/img/intro01.png, assets/img/intro01@2x.png 2x, assets/img/intro01@3x.png 3x" />
-                            <img src="assets/img/intro01.png" alt="소개이미지">
+                            <source srcset="../assets/img/intro01.png, ../assets/img/intro01@2x.png 2x, ../assets/img/intro01@3x.png 3x" />
+                            <img src="../assets/img/intro01.png" alt="소개이미지">
                         </picture> 
                         <p class="text">
                             어떤 일이라도 노력하고 즐기면 그 결과는 빛을 바란다고 생각합니다.
@@ -227,12 +177,8 @@
     </main>
     <!-- //main -->
 
-    <footer id="footer">
-        <div class="footer__inner container btStyle mb100">
-            <div>Copyright @ 2023 kebab00</div>
-            <p>blog by <a href="mailto:im.kebab00@gmail.com">im.kebab00@gmail.com</a></p>
-        </div>
-    </footer>
+    <?php include "../include/footer.php" ?>
     <!-- //footer -->
-</body>
+    
+    </body>
 </html>
